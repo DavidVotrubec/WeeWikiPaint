75aef086ea27aee73182f890201e37104edd9f9b
David <david@david-Pc-Ubuntu.(none)>
2012-10-06 10:11:35 +0200

    Lint is configured and working.

--- jakefile.js
+++ jakefile.js
@@ -1,7 +1,43 @@
+/*global desc, task, jake, fail, complete */
+
+(function() {
+
+"use strict";
+
 task("default", ["lint"]);
 
 desc("Lint everything");
+
 task("lint", [], function() {
    var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("jakefile.js", {});
+
+   var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+
+    var options = nodeLintOptions();
+
+    lint.validateFileList(files.toArray(), options, {});
 });
+
+function nodeLintOptions() {
+    return {
+        bitwise:true,
+        curly:false,
+        eqeqeq:true,
+        forin:true,
+        immed:true,
+        latedef:true,
+        newcap:true,
+        noarg:true,
+        noempty:true,
+        nonew:true,
+        regexp:true,
+        undef:true,
+        strict:true,
+        trailing:true,
+        node:true
+    };
+}
+
+})();
